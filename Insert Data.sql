fe4ca0d47eb7038159d3469bfc6c5550226b3fc1
Jose <asdf>
2021-11-15 15:37:36 -0300

    JQ se agrega archivo de insert

---
+++ Insert Data.sql
@@ -0,0 +1,4 @@
+select * from servicio;
+
+insert into servicio (codigoProgramador, premium, premiumHD, fechaAltaPremium, fechaAltaPremiumHD, fechaBajaPremium, fechaBajaPremiumHD) 
+values (1, 1, 1, '20120618 10:34:09 AM', '20120618 10:34:09 AM', '20120618 10:34:09 AM', '20120618 10:34:09 AM');
